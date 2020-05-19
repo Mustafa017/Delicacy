8c1d766a57ae0ab5484d05670ff357425642bc1e
Mustafa Khamisi <mustafangure@gmail.com>
2020-05-19 06:48:51 -0500

    feat: Add handlebars template system

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("express-handlebars");
+
 
 const app = express();
 
 app.use("/images",express.static(path.resolve(__dirname, './src/images/')));
 app.use("/css", express.static(path.resolve(__dirname, './src/css/')));
 app.use("/css", express.static(path.resolve(__dirname, './node_modules/bootstrap/dist/css/')));
-app.use("/js", express.static(path.resolve(__dirname, './node_modules/bootstrap/dist/js')));
-app.use("/js", express.static(path.resolve(__dirname, './node_modules/jquery/dist')));
+app.use("/js", express.static(path.resolve(__dirname, './node_modules/bootstrap/dist/js/')));
+app.use("/js", express.static(path.resolve(__dirname, './node_modules/jquery/dist/')));
+
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'main'
+}));
+
+// set views folder. otherwise express looks for the views folder in the root directory
+app.set('views',  path.resolve(__dirname,'./src/views/'));
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'src/views/index.html'));
+    res.render('home');
 });
 
 app.listen(3000, (err) => {
